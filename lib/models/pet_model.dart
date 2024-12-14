246f892ad5d5d9be01ca415ebe70053801594aeb
teukualdieaulia <teukualdiaulia@gmail.com>
2024-12-14 11:59:25 +0700

    Membuat Halaman Treatment & spa

---
+++ lib/models/pet_model.dart
@@ -0,0 +1,15 @@
+class Pet {
+  final String id;
+  final String name;
+  final String type;
+  final String breed;
+  final String imageUrl;
+
+  Pet({
+    required this.id,
+    required this.name,
+    required this.type,
+    required this.breed,
+    required this.imageUrl,
+  });
+}
